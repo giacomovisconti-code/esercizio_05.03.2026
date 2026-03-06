bf7b7cb91a07ac148eb56e633e4f489820afe8af
Giacomo Visconti <giacomo.visconti@zerodd.it>
2026-03-06 14:36:19 +0100

    Implementazione di api-gateway con routing base

---
+++ api-gateway/src/main/java/org/example/apigateway/GatewayConfig.java
@@ -0,0 +1,46 @@
+package org.example.apigateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    // Product Service
+    @Bean
+    public RouteLocator productService(RouteLocatorBuilder builder){
+        return builder.routes().route(p-> p.path("/api/products/**")
+                .uri("lb://PRODUCT-SERVICE")).build();
+    }
+
+    // User Service
+    @Bean
+    public RouteLocator userService(RouteLocatorBuilder builder){
+        return builder.routes().route(p-> p.path("/api/users/**")
+                .uri("lb://USER-SERVICE")).build();
+    }
+
+    // Inventory Service
+    @Bean
+    public RouteLocator inventoryService(RouteLocatorBuilder builder){
+        return builder.routes().route(p-> p.path("/api/inventory/**")
+                .uri("lb://INVENTORY-SERVICE")).build();
+    }
+
+    // Notification Service
+    @Bean
+    public RouteLocator notificationsService(RouteLocatorBuilder builder){
+        return builder.routes().route(p-> p.path("/api/notification/**")
+                .uri("lb://NOTIFICATION-SERVICE")).build();
+    }
+
+    // Order Service
+    @Bean
+    public RouteLocator ordersService(RouteLocatorBuilder builder){
+        return builder.routes().route(p-> p.path("/api/orders/**")
+                .uri("lb://ORDER-SERVICE")).build();
+    }
+
+}
