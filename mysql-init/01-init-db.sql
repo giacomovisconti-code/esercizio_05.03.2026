910b44a0c6800ad9bc6fe63221ae377de6b70be0
Giacomo Visconti <giacomo.visconti@zerodd.it>
2026-03-20 12:49:38 +0100

    create file .env to set the passwords

---
+++ mysql-init/01-init-db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS esercizio_users;
+CREATE DATABASE IF NOT EXISTS esercizio_orders;
+CREATE DATABASE IF NOT EXISTS esercizio_products;
+CREATE DATABASE IF NOT EXISTS esercizio_notifications;
+CREATE DATABASE IF NOT EXISTS esercizio_inventory;
